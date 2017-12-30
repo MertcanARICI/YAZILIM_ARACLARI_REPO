aa7d12f8385707ab006cc4158c6cda3b3c78175c
M.ARİCİ <mertcan.aricii@gmail.com>
2017-12-30 14:14:56 +0300

    SporingBootOrnek

---
+++ dSpringBootOrnek/src/main/java/com/medipol/dSpringBootOrnek/deneme.java
@@ -0,0 +1,13 @@
+package com.medipol.dSpringBootOrnek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class deneme {
+
+	public static void main(String[] args) {
+		SpringApplication.run(deneme.class, args);
+	}
+}
+
---
+++ dSpringBootOrnek/src/test/java/com/medipol/dSpringBootOrnek/DSpringBootOrnekApplicationTests.java
@@ -0,0 +1,16 @@
+package com.medipol.dSpringBootOrnek;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DSpringBootOrnekApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
